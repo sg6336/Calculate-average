c51b26babf91ddfa933795af76a80759d7064e80
Denys <navrotskyidenys@gmail.com>
2022-08-26 15:57:12 +0300

    Add all files of VS

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = new double[] { 17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16 };
+            AverageSolution.FindAverage(array);
+
+            // Should return 0 on empty array
+            AverageSolution.FindAverage(new double[] { });
+        }
+    }
+
+    public class AverageSolution
+    {
+        public static double FindAverage(double[] array)
+        {
+            if (array.Length == 0)
+            {
+                return 0;
+            }
+            double sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            double result = sum / array.Length;
+            return result;
+        }
+    }
+}
